6f6eb41add847569fbb81e00c5bca14002986ac5
antihax <antihax@gmail.com>
2023-07-17 10:23:37 -0500

    clean up and document

--- scripts/engine/radionotifications.c
+++ scripts/engine/radionotifications.c
@@ -33,17 +33,20 @@ class RadioNotificationEvent {
 	}
 
 	bool SerializeRPC(Serializer ctx) {
+		// Pack the 8-bit headers into 32-bit integer
 		ctx.Write((preamble & 0xFF) << 24 | (voice & 0xFF) << 16 | (noise & 0xFF) << 8 | signature & 0xFF);
 		ctx.Write(position);
 
-		// Pad with empty phonetics
+		// Pad with empty phonetics until we have a multiple of 4
+		// Since DayZ only allows sending 32 bit numbers we can align the phonetics to 32 bit
 		while (phonetics.Count() % 4) {
-			phonetics.Insert(-127);
+			phonetics.Insert(255);
 		}
 
-		ctx.Write((pause & 0xFFFF) << 16) | (phonetics.Count() / 4 & 0xFFFF);
+		// Pack the pause and packed phonetics length as 16 bit into 32-bit integer
+		ctx.Write((pause & 0xFFFF) << 16) | ((phonetics.Count() / 4) & 0xFFFF);
 
-		// Pack the phonetics into 32-bit integers
+		// Pack the 8-bit phonetics into 32-bit integers
 		for (int i = 0; i < phonetics.Count(); i += 4) {
 			ctx.Write((phonetics[i] & 0xFF) << 24 | (phonetics[i + 1] & 0xFF) << 16 | (phonetics[i + 2] & 0xFF) << 8 | phonetics[i + 3] & 0xFF);
 		}
@@ -54,13 +57,14 @@ class RadioNotificationEvent {
 	bool DeserializeRPC(Serializer ctx) {
 		int p, len;
 
+		// Unpack header 8-bit headers from 32-bit integer
 		ctx.Read(p);
 		preamble = (p >> 24) & 0xFF;
 		voice = (p >> 16) & 0xFF;
 		noise = (p >> 8) & 0xFF;
 		signature = p & 0xFF;
-		ctx.Read(p);
 
+		ctx.Read(p);
 		pause = (p >> 16) & 0xFFFF;
 		len = p & 0xFFFF;
 
@@ -74,16 +78,4 @@ class RadioNotificationEvent {
 
 		return true;
 	}
-
-	/* for reference so i don't forget the order
-	 int PackInt(cr a, char b, char c, char d) {
-		 return (a & 0xFF << 24) | (b  & 0xFF << 16) | (c & 0xFF << 8) | d & 0xFF;
-	 }
-
-	 void unpack_int(int p, int a, int b, int c, int d) {
-		 a = (p >> 24) & 0xFF;
-		 b = (p >> 16) & 0xFF;
-		 c = (p >> 8) & 0xFF;
-		 d = p & 0xFF;
-	 }*/
 }
